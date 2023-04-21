c97620e42bbaaac99903bffaf6e98c6afabc45ae
shubham kshirsagar <117492782+Shubhamkshirsagar1@users.noreply.github.com>
2023-04-21 10:45:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 //your JS code here. If required.
+let firstName = document.getElementById('firstName');
+let lastName = document.getElementById('lastName');
+let phoneNumber = document.getElementById('phoneNumber');
+let email = document.getElementById('email');
+let submitBtn = document.getElementById('submitBtn');
+
+function onFormClick(){
+	let firstNameValue = firstName.value;
+	let lastNameValue = lastName.value;
+	let phoneNumberValue = phoneNumber.value;
+	let emailValue = email.value;
+	console.log(firstNameValue)
+
+	if(firstNameValue != "" && lastNameValue != "" && phoneNumberValue != "" && emailValue != ""){
+		
+		alert(`First Name: ${firstNameValue}  Last Name: ${lastNameValue}  Phone Number: ${phoneNumberValue}  Email: ${emailValue}`)
+	}
+}
+
+submitBtn.addEventListener('click' , onFormClick)
+
+
+
